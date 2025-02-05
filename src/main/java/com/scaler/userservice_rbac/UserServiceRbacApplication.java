0fcc070992d96f1de600de49e4230cfe9624a28c
jnsheril <jnsheril@gmail.com>
2025-02-05 23:12:59 +0530

    project setup in springboot

---
+++ src/main/java/com/scaler/userservice_rbac/UserServiceRbacApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.userservice_rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceRbacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/userservice_rbac/UserServiceRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.userservice_rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceRbacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
